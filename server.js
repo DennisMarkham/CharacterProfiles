a89e7cf16f039ca3469dc84b4d189c7bcfb6c00b
unknown <workdennis@yahoo.com>
2019-07-27 18:18:43 -0500

    added a handlebars version

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var exphbs = require("express-handlebars");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Non-handlebars version
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+//****************
+
+//handlebars version
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+//*****************
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
@@ -19,32 +26,49 @@ app.listen(PORT, function() {
 var characters = [
 {
 name: "Mordecia",
-style: "body {background-color: blue;}"
+style: "body {background-color: blue;}",
+species: "bluejay"
 }, 
 {name: "Rigby",
-style: "body {background-color: brown;}"
+style: "body {background-color: brown;}",
+species: "raccoon"
 },
 {name: "Benson",
-style: "body {background-color: red;}"}
+style: "body {background-color: red;}",
+species: "gumball machine"
+}
 ];
 
 app.get("/:characters?", function(req, res) {
   var chosen = req.params.characters;
 
 
+  // NON-HANDLEBARS VERSION
+  // if (chosen) {
+  //   console.log(chosen);
 
-  if (chosen) {
-    console.log(chosen);
+  //   for (var i = 0; i < characters.length; i++) {
+  //     if (chosen.toLowerCase() === characters[i].name.toLowerCase()) {
+  //        var html = "<head> <style>" + characters[i].style + "</style> </head> <body>"
+  //        html += "<h1>" + characters[i].name + "</h1></body>";
+  //       return res.send(html);
+  //     }
+  //   }
+  //   //not sure what this does
+  //   return res.json(false);
+  // }
+  // return res.json(characters);
+  //**************
 
-    for (var i = 0; i < characters.length; i++) {
+//HANDLE BARS VERSION  
+if (chosen)
+{
+  for (var i = 0; i < characters.length; i++) {
       if (chosen.toLowerCase() === characters[i].name.toLowerCase()) {
-         var html = "<head> <style>" + characters[i].style + "</style> </head> <body>"
-         html += "<h1>" + characters[i].name + "</h1></body>";
-        return res.send(html);
+         
+        return res.render("character", characters[i]);
       }
     }
-    //not sure what this does
-    return res.json(false);
-  }
-  return res.json(characters);
+}
+//*************
 });
